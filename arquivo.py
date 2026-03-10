a638b0ab218f2d67ace62f1643e07669a6f124f4
Brookzz64 <nygaardathos@gmail.com>
2026-03-10 14:21:30 -0300

    Implement discipline management functions in arquivo.py
    used to save information in a diferent file on your pc, to avoid loose of information when leaving the program.

---
+++ arquivo.py
@@ -0,0 +1,52 @@
+import csv
+import model
+
+def salvar_disciplina(disciplina):
+    with open("dados.csv", "a", newline="", encoding="utf-8") as arquivo:
+        escritor = csv.writer(arquivo)
+        final = disciplina.final
+        if final == None:
+            final = -1
+        escritor.writerow([disciplina.nome, disciplina.av1, disciplina.av2, disciplina.av3, disciplina.edag, disciplina.media, disciplina.situacao, final])
+
+def ler_disciplina(nome_disciplina):
+    with open("dados.csv", "r", encoding="utf-8") as arquivo:
+        leitor = csv.reader(arquivo)
+        for linha in leitor:
+            if linha[0] == nome_disciplina:
+                disciplina = model.Disciplina(linha[0], linha[1], linha [2], linha[3], linha[4])
+                disciplina.media = float(linha[5])
+                disciplina.situacao = linha[6]
+                final = float(linha[7])
+                if linha[7] == -1:
+                    final = None
+                disciplina.final = final
+                return disciplina
+        return None
+    
+def ler_todas_disciplinas():
+    disciplinas = []
+    with open("dados.csv", "r", encoding="utf-8") as arquivo:
+        leitor = csv.reader(arquivo)
+        for linha in leitor:
+            disciplina = model.Disciplina(linha[0], linha[1], linha [2], linha[3], linha[4])
+            disciplina.media = float(linha[5])
+            disciplina.situacao = linha[6]
+            final = float(linha[7])
+            if linha[7] == -1:
+                final = None
+            disciplina.final = final
+            disciplinas.append(disciplina)
+    return disciplinas
+
+def remover_disciplina(nome_disciplina):
+    linhas = []
+    with open("dados.csv", "r", encoding="utf-8") as arquivo:
+        leitor = csv.reader(arquivo)
+        linhas = list(leitor)
+
+    novas_linhas = [l for l in linhas if l[0].upper() != nome_disciplina.upper()]
+
+    with open("dados.csv", "w", newline="", encoding="utf-8") as arquivo:
+        escritor = csv.writer(arquivo)
+        escritor.writerows(novas_linhas)
