059f1e9e8e16e45d1ac0d432b29a892f378420ca
Brookzz64 <nygaardathos@gmail.com>
2026-03-10 14:16:11 -0300

    Add functions to read float and string inputs
    its dedicated to simplify the model code, creating generic funtions to help with organization on the code.

---
+++ util.py
@@ -0,0 +1,10 @@
+def ler_float(num):    
+   while True:
+    try:
+        return float(input(num))
+    except ValueError:
+        print("Entrada inválida, digite um número.")
+    
+def ler_str(nome):   
+
+    return input(nome)
